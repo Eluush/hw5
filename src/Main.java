4786d159530630c781229c8f90c7a82d6df1ae11
79774 <Cinnabon_09@.com>
2024-12-14 21:24:52 +0300

    hw5 done.

--- src/Main.java
+++ src/Main.java
@@ -7,82 +7,88 @@ public class Main {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
 
-        int clientDeviceYear=2014;
+        int clientDeviceYear = 2014;
         if (clientOs == 0 && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        }else if (clientOs == 0 && clientDeviceYear >= 2015){
+        } else if (clientOs == 0 && clientDeviceYear >= 2015) {
             System.out.println("Установите версию приложения для iOS по ссылке");
-        }else if (clientOs==1 && clientDeviceYear < 2015){
+        } else if (clientOs == 1 && clientDeviceYear < 2015) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке ");
-        }else  {
+        } else {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
 
 
         int year = 2024;
-        if (year % 4 == 0 || year % 400 == 0) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
             System.out.println(year + " год является високосным");
-        } else if (year % 100 == 0) {
+        } else {
             System.out.println(year + " год не является високосным");
         }
 
         int deliveryDistance = 95;
         int deliveryDays = 1;
         if (deliveryDistance > 100) {
-            System.out.println("Доставки нет");
+            System.out.println("Нет доставки");
             return;
         }
         if (deliveryDistance > 20) {
             deliveryDays++;
-        } else if (deliveryDistance > 60) {
-            deliveryDays++;
-        } {
-        System.out.println("Доставка займет " + deliveryDays + " суток");
-    }
-
-        int monthNumber = 12;
-        if (monthNumber > 12)
-            System.out.println("Недопустимое число");
-        switch (monthNumber) {
-            case 1:
-                System.out.println("Зима");
-                break;
-            case 2:
-                System.out.println("Зима");
-                break;
-            case 3:
-                System.out.println("Весна");
-                break;
-            case 4:
-                System.out.println("Весна");
-                break;
-            case 5:
-                System.out.println("Весна");
-                break;
-            case 6:
-                System.out.println("Лето");
-                break;
-            case 7:
-                System.out.println("Лето");
-                break;
-            case 8:
-                System.out.println("Лето");
-                break;
-            case 9:
-                System.out.println("Осень");
-                break;
-            case 10:
-                System.out.println("Осень");
-                break;
-            case 11:
-                System.out.println("Осень");
-                break;
-            case 12:
-                System.out.println("Зима");
-                break;
         }
+        if (deliveryDistance > 60) {
+            deliveryDays++;
+        }
+        System.out.println("Доставка займет " + deliveryDays + " суток");
 
 
+
+         int monthNumbers = 12;
+         switch(monthNumbers) {
+             case 1:
+                 System.out.println("Зима");
+                 break;
+             case 2:
+                 System.out.println("Зима");
+                 break;
+             case 3:
+                 System.out.println("Весна");
+                 break;
+             case 4:
+                 System.out.println("Весна");
+                 break;
+             case 5:
+                 System.out.println("Весна");
+                 break;
+             case 6:
+                 System.out.println("Лето");
+                 break;
+             case 7:
+                 System.out.println("Лето");
+                 break;
+             case 8:
+                 System.out.println("Лето");
+                 break;
+             case 9:
+                 System.out.println("Осень");
+                 break;
+             case 10:
+                 System.out.println("Осень");
+                 break;
+             case 11:
+                 System.out.println("Осень");
+                 break;
+             case 12:
+                 System.out.println("Зима");
+                 break;
+             default:
+                 System.out.println("Пользователь ввел недопустимое значение");
+         }
     }
 
 }
+
+
+
+
+
+
